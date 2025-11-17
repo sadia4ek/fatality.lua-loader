e3575acab7852becd34df95020856131b10ee1c8
sadia4ek <kapybara1122@gmail.com>
2025-11-17 23:28:35 +0200

    Create startloader.lua

---
+++ startloader.lua
@@ -0,0 +1,25 @@
+local function loadFatalityGUI()
+    local modelUrl = "https://raw.githubusercontent.com/sadia4ek/fatality.lua-loader/main/loader.rbxm"
+    
+    local success, modelData = pcall(function()
+        return game:HttpGet(modelUrl)
+    end)
+    
+    if not success then
+        warn("error:", modelData)
+        return
+    end
+    
+    local model = game:GetService("InsertService"):LoadLocalAsset(modelData)
+    
+    if model then
+        local guiClone = model:FindFirstChildOfClass("ScreenGui"):Clone()
+        guiClone.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+        
+        print("succes")
+    else
+        warn("error")
+    end
+end
+
+loadFatalityGUI()
